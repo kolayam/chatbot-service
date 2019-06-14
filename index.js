1bae0444f2170d8883b410b1482bed523e836633
sshniro <sshniro@gmail.com>
2019-06-14 09:39:30 +0200

    Adding endpoint to search for channel based on product id and company details

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = 8090;
+const port = 8095;
 var axios = require('axios');
 var fs = require('fs');
 var bodyParser = require('body-parser');
@@ -26,9 +26,26 @@ app.get('/hello', (req, res) => {
 });
 
 app.post('/channel', async (req, res) => {
+    console.log("request received to search for create channel: " + req.body);
     mongoHelper.insert({collection: 'nimble', data: req.body}).then(response => {
-        res.status(200);
+        res.status(201);
         res.send();
+    }).catch(err => {
+        res.status(400);
+        res.send()
+    })
+});
+
+app.post('/channels', async (req, res) => {
+    console.log("request received to search for channels: " + req.body);
+    mongoHelper.getItem({collection: 'nimble', data: req.body}).then(response => {
+        if (response.length !== 0) {
+            res.status(200);
+            res.send({channelName: response[0]['channelName']});
+        }else {
+            res.status(404);
+            res.send();
+        }
     }).catch(err => {
         res.status(400);
         res.send()
