486cef2b739748f94aa323aece76ea1fcfaa3c67
sshniro <sshniro@gmail.com>
2019-06-18 14:14:51 +0200

    fix for channel creation bug

--- index.js
+++ index.js
@@ -38,13 +38,35 @@ app.post('/channel', async (req, res) => {
 
 app.post('/channels', async (req, res) => {
     console.log("request received to search for channels: " + req.body);
-    mongoHelper.getItem({collection: 'nimble', data: req.body}).then(response => {
-        if (response.length !== 0) {
+    let channelData = {};
+    let initiatingPartyID = req.body.initiatingPartyID;
+    let respondingPartyID = req.body.respondingPartyID;
+
+    channelData.catalogueID = req.body.catalogueID;
+    channelData.initiatingPartyID = initiatingPartyID;
+    channelData.respondingPartyID = respondingPartyID;
+
+
+    mongoHelper.getItem({collection: 'nimble', data: channelData}).then(firstResponse => {
+        console.log("Attempting first query" + JSON.stringify(channelData));
+        if (firstResponse.length !== 0) {
             res.status(200);
-            res.send({channelName: response[0]['channelName']});
+            res.send({channelName: firstResponse[0]['channelName']});
         }else {
-            res.status(404);
-            res.send();
+            // modify query
+            channelData.initiatingPartyID = respondingPartyID;
+            channelData.respondingPartyID = initiatingPartyID;
+
+            mongoHelper.getItem({collection: 'nimble', data: channelData}).then(secondResponse => {
+                console.log("response not found, making second call, " + JSON.stringify(channelData));
+                if (secondResponse.length !== 0) {
+                    res.status(200);
+                    res.send({channelName: secondResponse[0]['channelName']});
+                }else {
+                    res.status(404);
+                    res.send();
+                }
+            });
         }
     }).catch(err => {
         res.status(400);
